555ad553ef9979135299651194b07f4d4ab5955d
Alexander Potashev <aspotashev@gmail.com>
2010-05-01 22:31:11 +0400

    set white color

--- glwidget.cpp
+++ glwidget.cpp
@@ -29,6 +29,8 @@ void GLWidget::initializeGL()
 void GLWidget::paintGL()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glColor3f(1, 1, 1);
+
 	GLUquadricObj *quad = gluNewQuadric();
 
 
